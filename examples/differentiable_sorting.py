85fa0aeaf95f3932527f7d725197a2734bfc8262
wangshanyw <Zhangruohan@sjtu.edu.cn>
2024-05-26 22:06:09 -0700

    finish bubble_sort and ranking func in loma

---
+++ examples/differentiable_sorting.py
@@ -0,0 +1,80 @@
+import os
+import sys
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+import compiler
+import ctypes
+import numpy as np
+import matplotlib.pyplot as plt
+
+def test_ascending_bubble_sort(arr, lib):
+    size = len(arr)
+    reverse = 0
+    sorted_arr = np.zeros(size, dtype=np.float32)
+    lib.bubble_sort(arr.ctypes.data_as(ctypes.POINTER(ctypes.c_float)), size, reverse, sorted_arr.ctypes.data_as(ctypes.POINTER(ctypes.c_float)))
+    py_sorted_arr = np.sort(arr)
+    for i in range(size):
+        assert sorted_arr[i] == py_sorted_arr[i]
+
+def test_descending_bubble_sort(arr, lib):
+    size = len(arr)
+    reverse = 1
+    sorted_arr = np.zeros(size, dtype=np.float32)
+    lib.bubble_sort(arr.ctypes.data_as(ctypes.POINTER(ctypes.c_float)), size, reverse, sorted_arr.ctypes.data_as(ctypes.POINTER(ctypes.c_float)))
+    py_sorted_arr = np.sort(arr)[::-1]
+    for i in range(size):
+        assert sorted_arr[i] == py_sorted_arr[i]
+
+def test_argsort_increasing_order(arr, lib):
+    size = len(arr)
+    reverse = 0
+    rank = np.zeros(size, dtype=np.int32)
+    lib.argsort_bubble_sort(arr.ctypes.data_as(ctypes.POINTER(ctypes.c_float)), size, reverse, rank.ctypes.data_as(ctypes.POINTER(ctypes.c_int)))
+    py_rank = np.argsort(arr)
+    for i in range(size):
+        assert rank[i] == py_rank[i]
+
+def test_argsort_decreasing_order(arr, lib):
+    size = len(arr)
+    reverse = 1
+    rank = np.zeros(size, dtype=np.int32)
+    lib.argsort_bubble_sort(arr.ctypes.data_as(ctypes.POINTER(ctypes.c_float)), size, reverse, rank.ctypes.data_as(ctypes.POINTER(ctypes.c_int)))
+    py_rank = np.argsort(arr)[::-1]
+    for i in range(size):
+        assert rank[i] == py_rank[i]
+
+def test_stable_argsort_increasing_order(arr, lib):
+    size = len(arr)
+    reverse = 0
+    rank = np.zeros(size, dtype=np.int32)
+    lib.argsort_bubble_sort(arr.ctypes.data_as(ctypes.POINTER(ctypes.c_float)), size, reverse, rank.ctypes.data_as(ctypes.POINTER(ctypes.c_int)))
+    py_rank = np.argsort(arr, kind='stable')
+    for i in range(size):
+        assert rank[i] == py_rank[i]
+
+def test_stable_argsort_decreasing_order(arr, lib):
+    size = len(arr)
+    reverse = 1
+    rank = np.zeros(size, dtype=np.int32)
+    lib.argsort_bubble_sort(arr.ctypes.data_as(ctypes.POINTER(ctypes.c_float)), size, reverse, rank.ctypes.data_as(ctypes.POINTER(ctypes.c_int)))
+    py_rank = size - 1 - np.argsort(arr[::-1], kind='stable')[::-1]
+    for i in range(size):
+        assert rank[i] == py_rank[i]
+
+if __name__ == '__main__':
+    with open('loma_code/differentiable_sorting.py') as f:
+        structs, lib = compiler.compile(f.read(),
+                                  target = 'c',
+                                  output_filename = '_code/differentiable_sorting')
+
+    arr = np.array([64.2, 34.5, 25.5, 12.22, 11.0, 90.1]).astype(np.float32)
+    test_ascending_bubble_sort(arr, lib)
+    test_descending_bubble_sort(arr, lib)
+    test_argsort_increasing_order(arr, lib)
+    test_argsort_decreasing_order(arr, lib)
+
+    arr = np.array([3.0, 1.0, 2.0, 3.0, 3.0]).astype(np.float32)
+    test_stable_argsort_increasing_order(arr, lib)
+    test_stable_argsort_decreasing_order(arr, lib)
+    print('All tests passed')
---
+++ examples/loma_code/differentiable_sorting.py
@@ -0,0 +1,84 @@
+
+# input an array
+# output the sorted array
+
+# RaMBO: Blackbox differentiation for ranking
+
+
+
+# use loma to implement a typical sorting function and ranking function
+# reverse //
+def bubble_sort(arr : In[Array[float]], arr_size : In[int], reverse: In[int], sorted_arr: Out[Array[float]]):
+    i : int = 0
+    j : int = 0
+    tmp : float = 0.0
+    swapped: int = 0
+    while (i < arr_size, max_iter := 10000):
+        sorted_arr[i] = arr[i]
+        i = i + 1
+    
+    i = 0
+    while (i < arr_size, max_iter := 10000):
+        j = 0
+        swapped = 0
+        while (j < arr_size - i - 1, max_iter := 10000):
+            if reverse == 1:
+                if sorted_arr[j] < sorted_arr[j + 1]:
+                    tmp = sorted_arr[j]
+                    sorted_arr[j] = sorted_arr[j + 1]
+                    sorted_arr[j + 1] = tmp
+                    swapped = 1
+            else:
+                if sorted_arr[j] > sorted_arr[j + 1]:
+                    tmp = sorted_arr[j]
+                    sorted_arr[j] = sorted_arr[j + 1]
+                    sorted_arr[j + 1] = tmp
+                    swapped = 1
+            j = j + 1
+        i = i + 1
+        if swapped == 0:
+            i = arr_size
+
+def argsort_bubble_sort(arr : In[Array[float]], arr_size : In[int], reverse: In[int], rank: Out[Array[int]]):
+    i : int = 0
+    j : int = 0
+    tmp_index: int = 0
+    swapped: int = 0
+
+    while (i < arr_size, max_iter := 10000):
+        rank[i] = i
+        i = i + 1
+    i = 0
+    while (i < arr_size, max_iter := 10000):
+        j = 0
+        swapped = 0
+        while (j < arr_size - i - 1, max_iter := 10000):
+            if reverse == 1:
+                if arr[rank[j]] < arr[rank[j + 1]]:
+                    tmp_index = rank[j]
+                    rank[j] = rank[j + 1]
+                    rank[j + 1] = tmp_index
+                    swapped = 1
+            else:
+                if arr[rank[j]] > arr[rank[j + 1]]:
+                    tmp_index = rank[j]
+                    rank[j] = rank[j + 1]
+                    rank[j + 1] = tmp_index
+                    swapped = 1
+            j = j + 1
+        i = i + 1
+        if swapped == 0:
+            i = arr_size
+    
+
+
+# def ranker(arr : In[Array[float]], arr_size : In[int]) -> float:
+#     i : int = 0
+#     s : float = 0.0
+#     while (i < arr_size, max_iter := 1000):
+#         s = s + arr[i]
+#         i = i + 1
+#     s_relu : float = 0.0
+#     if s > 0:
+#     	s_relu = s
+#     return s_relu
