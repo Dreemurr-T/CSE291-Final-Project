ce3854d79b0c4d3447c77b17d86091a09251dd9f
THY <h2tian@ucsd.edu>
2024-05-28 23:10:17 -0700

    update rev diff RaMBO

--- diff_sorting/loma_code/diff_RaMBO.py
+++ diff_sorting/loma_code/diff_RaMBO.py
@@ -1,75 +1,61 @@
 # autodiff version of RaMBO: Blackbox differentiation for ranking
-class Rank:
-    score : Array[float, 10000]
-    rank: Array[int, 10000]
-    norm_rank : Array[float, 10000]
-    size : int
-#     _lambda : float
 
-def ranker(r : In[Rank], reverse : In[int]) -> Rank:
+def ranker(arr : In[Array[float]], arr_size : In[int], reverse: In[int], rank: Out[Array[int]]):
     i : int = 0
     j : int = 0
     cnt : int = 0
-    while (i < r.size, max_iter := 10000):
+    while (i < arr_size, max_iter := 10000):
         cnt = 0
         j = 0
-        while (j < r.size, max_iter := 10000):
+        while (j < arr_size, max_iter := 10000):
             if reverse == 1:
-                if r.score[j] > r.score[i]:
+                if arr[j] > arr[i]:
                     cnt = cnt + 1
             else:
-                if r.score[j] < r.score[i]:
+                if arr[j] < arr[i]:
                     cnt = cnt + 1
             j = j + 1
-        r.score[i] = cnt
+        rank[i] = cnt
         i = i + 1
 
-# d_argsort_bubble_sort = fwd_diff(argsort_bubble_sort)
 
-def normalize_rank(r : In[Rank]) -> Rank:
+def normalize_rank(rank : In[Array[int]], size : In[int], norm_rank : Out[Array[float]]):
     i : int = 0
-    while (i < r.size, max_iter := 10000):
-        r.norm_rank[i] = (r.rank[i] + 1) / (r.size + 1e-8)
+    while (i < size, max_iter := 10000):
+        norm_rank[i] = (rank[i] + 1.0) / (size + 1e-8)
         i = i + 1
-    
-    return r
 
-# d_normaliza_rank = fwd_diff(normalize_rank)
 
-def RaMBO_forward(r : In[Rank]) -> Rank:
-    r = ranker(r, 0)
-    r = normalize_rank(r)
+def RaMBO_forward(score : In[Array[float]], size : In[int], rank : Out[Array[int]], norm_rank : Out[Array[float]]):
+    ranker(score, size, 1, rank)
+    normalize_rank(rank, size, norm_rank)
 
-    # Loss function here
-
-    return r
-
-d_fwd_RaMBO_forward = fwd_diff(RaMBO_forward)
-
-def fwd_diff_RaMBO_backward(r : In[Rank], lambda_val : In[float]) -> Array[Diff[float], 10000]:
-    d_r : Diff[Rank]
+# not finished
+def recall_loss(norm_rank : In[Array[float]], size : In[int], label : In[Array[int]], loss : Out[float]):
     i : int = 0
-    d_r.size = r.size
-
-    while (i < r.size, max_iter := 10000):
-        d_r.score[i].val = r.score[i]
-        # d_r.score[i].dval = 0.0
-        # d_r.rank[i] = i
-        # d_r.norm_rank[i].val = 0.0
-        # d_r.norm_rank[i].dval = 0.0
-
+    while (i < size, max_iter := 10000):
+        loss = loss + (label[i] - norm_rank[i]) * (label[i] - norm_rank[i])
         i = i + 1
 
-    d_r = d_fwd_RaMBO_forward(d_r)
+d_rev_recall_loss = rev_diff(recall_loss)
+
+# output gradient
+def RaMBO_backward(score : In[Array[float]], score_prime : In[Array[float]], 
+                   size : In[int], rank : In[Array[int]], rank_prime : In[Array[int]], norm_rank : In[Array[float]], norm_rank_prime : In[Array[float]], 
+                   d_norm_rank : In[Array[float]], label : In[Array[int]], d_label : In[Array[int]], lambda_val : In[float], gradient : Out[Array[float]]):
     
-    while (i < r.size, max_iter := 10000):
-        r.score[i] = r.score[i] + lambda_val * d_r.norm_rank[i].dval
-        i = i + 1
+    i : int
+    RaMBO_forward(score, size, rank, norm_rank)
 
-    r = RaMBO_forward(r)
-
-    while (i < d_r.size, max_iter := 10000):
-        d_r.score[i].dval = -(d_r.norm_rank[i].val - r.norm_rank[i]) / (lambda_val + 1e-8)
-        i = i + 1
+    d_size : int
+    loss : float
+    d_rev_recall_loss(norm_rank, d_norm_rank, size, d_size, label, d_label, loss)
     
-    return d_r.score
+    while (i < size, max_iter := 10000):
+        score_prime[i] = score[i] + lambda_val * d_norm_rank[i]
+
+    RaMBO_forward(score_prime, size, rank_prime, norm_rank_prime)
+
+    while (i < size, max_iter := 10000):
+        gradient[i] = -(norm_rank_prime[i] - norm_rank[i]) / (lambda_val + 1e-8)
+        i = i + 1
